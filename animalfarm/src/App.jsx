d624b69d5f4387bafd3dad0a517232cb95c668bf
michaelm3 <michael.muniz94@gmail.com>
2022-05-11 14:19:44 -0400

    finished animalfarm search

--- animalfarm/src/App.jsx
+++ animalfarm/src/App.jsx
@@ -1,19 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Animal from './components/Animal'
 
 function App() {
+    const { search, animals } = useAnimalSearch()
 
-  return (
-    <main>
-        <h1>Animal Farm</h1>
+    return (
+        <main>
+            <h1>Animal Farm</h1>
 
-        <input
-            type='text'
-            placeholder='Search'
-            onChange={(e) => console.log(e.target.value)}
-        />
-    </main>
-  )
+            <input
+                type='text'
+                placeholder='Search'
+                onChange={(e) => search(e.target.value)}
+            />
+            <ul>
+                {animals.map((animal) => (
+                    <Animal key={animal.id} {...animal} />
+                ))}
+
+                {animals.length === 0 && 'No animals found'}
+            </ul>
+        </main>
+    )
+}
+
+// Custom Hook
+function useAnimalSearch() {
+    const [animals, setAnimals] = useState([])
+
+    // Component Did Mount
+    useEffect(() => {
+        const lastQuery = localStorage.getItem('lastQuery')
+        search(lastQuery)
+    }, [])
+
+    const search = async (q) => {
+        const res = await fetch(
+            'http://localhost:8080?' + new URLSearchParams({ q })
+        )
+        const data = await res.json()
+        setAnimals(data)
+
+        localStorage.setItem('lastQuery', q)
+    }
+    return { search, animals }
 }
 
 export default App
